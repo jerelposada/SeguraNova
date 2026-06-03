9fbbf5e3fe33776b1454af28607ac8832f1c477c
JerelPosada <jerel.posada@col.vueling.com>
2026-06-03 14:27:09 -0500

    feat: add dashboard component with routing and styles

--- SeguraNova.Spa/src/app/app.routes.ts
+++ SeguraNova.Spa/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {'path': 'login', 'loadComponent': () => import('./Pages/login/login.component').then(m => m.LoginComponent)},
+    {'path': 'dashboard', 'loadComponent': () => import('./Pages/dashboard/dashboard.component').then(m => m.DashboardComponent)},
     {'path': '**', 'redirectTo': 'login'},
     {'path': '', 'redirectTo': 'login', 'pathMatch': 'full'}
 ];
